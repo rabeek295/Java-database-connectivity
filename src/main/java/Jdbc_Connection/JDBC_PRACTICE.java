6162b35c641e9fa2dd7ca280ae6c2e28ee0cd447
rabeek295 <123293452+rabeek295@users.noreply.github.com>
2024-12-25 22:50:53 +0530

    final production

--- src/main/java/Jdbc_Connection/JDBC_PRACTICE.java
+++ src/main/java/Jdbc_Connection/JDBC_PRACTICE.java
@@ -8,8 +8,8 @@ package Jdbc_Connection;
 // 6. Process the result
 // 7. Close the connection
 
-// Excute methods
-    //1.if DDL use-> execute() -> Retrun type = boolean
+// Execute methods
+    //1.if DDL use-> execute() -> Return type = boolean
     //2.if DML use-> executeUpdate() -> Return type = ResultSet
     //3.if DQL use-> executeQuery() -> Return type = ClassType
 
